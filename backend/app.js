3ed97da053d00e1062a4c898890c920ec37b5a9e
yinonhever <yinonehever@gmail.com>
2021-01-13 23:56:51 +0200

    _

--- backend/app.js
+++ backend/app.js
@@ -18,9 +18,11 @@ app.use(cors());
 app.use("/api/users", userRoutes);
 app.use("/api/chats", chatRoutes);
 
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        const server = app.listen(5000, () => console.log("Server running on port 5000"));
+        const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
         const io = socket.init(server);
         io.on("connect", client => console.log(client.id));
     })
