5a990a0bb17aa6b0747eac80452731681c85e45c
yinonhever <yinonehever@gmail.com>
2021-01-25 02:17:22 +0200

    _

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:5000";
+export const baseURL = "https://chatter-bbq.herokuapp.com";
 
 export default axios.create({ baseURL });
